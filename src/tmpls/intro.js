2f58bc81328602132cb62f72e0ce0d0f2dd8f63b
Jarid Margolin <jaridmargolin@gmail.com>
2013-10-30 19:25:08 -0700

    update interface. 0.0.2

--- src/tmpls/intro.js
+++ src/tmpls/intro.js
@@ -1,4 +1,1 @@
 ;(function (window, document, undefined) {
-
-// Expose to window
-if (typeof window !== 'undefined') { window.BounceFix = BounceFix; }
--- src/tmpls/outro.js
+++ src/tmpls/outro.js
@@ -1,1 +1,4 @@
+// Expose to window
+if (typeof window !== 'undefined') { window.bouncefix = bouncefix; }
+
 })(window, document);
